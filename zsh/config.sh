c7f7e76d6e30bdd4e6783b6038e1f4ce82f61257
Pablo Cordero <pablocorderoromero@gmail.com>
2021-11-23 22:37:33 +0100

    zsh: solves #31

--- zsh/config.sh
+++ zsh/config.sh
@@ -3,3 +3,5 @@
 # Download auto-suggestions plugin
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
+# Copy zshrc file
+cp ~/.dotfiles/zsh/.zshrc ~/.zshrc
