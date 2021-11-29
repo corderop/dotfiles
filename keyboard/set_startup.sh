a53405c88dabdc5ff6dae5b10d07e93ed54caeb5
Pablo Cordero <pablocorderoromero@gmail.com>
2021-11-29 20:16:00 +0100

    keyboard: change behaviour to avoid problems with git changes

--- keyboard/set_startup.sh
+++ keyboard/set_startup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+cp layout.xkb ~/.layout.xkb
 cp keyboard-layout-start.desktop ~/.config/autostart/keyboard-layout-start.desktop
--- keyboard/start.sh
+++ keyboard/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sleep 2
-xkbcomp ~/.dotfiles/keyboard/layout.xkb ":0" > /dev/null 2>&1
+xkbcomp ~/.layout.xkb ":0" > /dev/null 2>&1
