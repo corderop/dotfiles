cd1ed7c187f450956b443616217898317e3b3f46
Pablo Cordero <pablocorderoromero@gmail.com>
2021-11-30 21:01:33 +0100

    keyboard: small fixes to keyboard layout starter

--- keyboard/start.sh
+++ keyboard/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sleep 2
-xkbcomp ~/.pre-layout.xkb ":0" > /dev/null 2>&1
-xkbcomp ~/.layout.xkb ":0" > /dev/null 2>&1
+xkbcomp ~/.layout/.pre-layout.xkb ":0"
+xkbcomp ~/.layout/.layout.xkb ":0"
