408f59e168a4438ed550802da04b566f32dff79b
Pablo Cordero <pablocorderoromero@gmail.com>
2022-08-11 19:59:26 +0200

    feat(so,linux): add all plasma/kde configuration

---
+++ so/linux/kde/konsole/Main.profile
@@ -0,0 +1,12 @@
+[Appearance]
+ColorScheme=Breath Light Custom
+Font=Noto Mono,10,-1,0,50,0,0,0,0,0
+
+[Cursor Options]
+CursorShape=1
+UseCustomCursorColor=false
+
+[General]
+Command=/bin/zsh
+Name=Main
+Parent=FALLBACK/
---
+++ symlinks/so.sh
@@ -0,0 +1,29 @@
+# GTK
+ln -s "$HOME/.dotfiles/so/linux/gtk/gtk-2.0" "$HOME/.config/gtk-2.0"
+ln -s "$HOME/.dotfiles/so/linux/gtk/gtk-3.0" "$HOME/.config/gtk-3.0"
+ln -s "$HOME/.dotfiles/so/linux/gtk/gtk-4.0" "$HOME/.config/gtk-4.0"
+
+# KDE
+ln -s "$HOME/.dotfiles/so/linux/kde/kglobalshortcutsrc" "$HOME/.config/kglobalshortcutsrc"
+ln -s "$HOME/.dotfiles/so/linux/kde/kwinrc" "$HOME/.config/kwinrc"
+ln -s "$HOME/.dotfiles/so/linux/kde/kxkbrc" "$HOME/.config/kxkbrc"
+ln -s "$HOME/.dotfiles/so/linux/kde/ksplashrc" "$HOME/.config/ksplashrc"
+ln -s "$HOME/.dotfiles/so/linux/kde/kdeglobals" "$HOME/.config/kdeglobals"
+ln -s "$HOME/.dotfiles/so/linux/kde/plasma/plasmashellrc" "$HOME/.config/plasmashellrc"
+ln -s "$HOME/.dotfiles/so/linux/kde/plasma/plasma-org.kde.plasma.desktop-appletsrc" "$HOME/.config/plasma-org.kde.plasma.desktop-appletsrc"
+ln -s "$HOME/.dotfiles/so/linux/kde/Trolltech.conf" "$HOME/.config/Trolltech.conf"
+
+## KXMLGUI5
+ln -s "$HOME/.dotfiles/so/linux/kde/kxmlgui5" "$HOME/.local/share/kxmlgui5"
+
+## Konsole
+ln -s "$HOME/.dotfiles/so/linux/kde/konsole/Main.profile" "$HOME/.local/share/konsole/Main.profile"
+ln -s "$HOME/.dotfiles/so/linux/kde/konsole/Breath Light Custom.colorscheme" "$HOME/.local/share/konsole/Breath Light Custom.colorscheme"
+ln -s "$HOME/.dotfiles/so/linux/kde/konsole/konsolerc" "$HOME/.config/konsolerc"
+
+## Plasma
+### Plasmoid
+ln -s "$HOME/.dotfiles/so/linux/kde/plasma/plasmoids" "$HOME/.local/share/plasma/plasmoids"
+
+## kdedefaults
+ln -s "$HOME/.dotfiles/so/linux/kde/kdedefaults" "$HOME/.config/kdedefaults"
