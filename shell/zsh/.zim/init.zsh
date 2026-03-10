70ae8c36664dd30feed26938999c5ea6243d3991
Pablo Cordero <pablocorderoromero@gmail.com>
2026-03-10 19:33:21 +0100

    fix: undo $USER change

--- shell/zsh/.zim/init.zsh
+++ shell/zsh/.zim/init.zsh
@@ -1,4 +1,4 @@
-# FILE AUTOMATICALLY GENERATED FROM /Users/$USER/.zimrc
+# FILE AUTOMATICALLY GENERATED FROM /Users/pcordero/.zimrc
 # EDIT THE SOURCE FILE AND THEN RUN zimfw build. DO NOT DIRECTLY EDIT THIS FILE!
 
 if [[ -e ${ZIM_CONFIG_FILE:-${ZDOTDIR:-${HOME}}/.zimrc} ]] zimfw() { source "${HOME}/.dotfiles/shell/zsh/.zim/zimfw.zsh" "${@}" }
